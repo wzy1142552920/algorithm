0acc707dd1c2415842478fc2c8048dc61fe0cd88
ZY Wei <wzy1142552920@outlook.com>
2022-06-07 23:09:11 +0800

    1、两数相除，向上取整 2、二分法计算中间位置 - 位运算（运算符优先级）

--- src/main/java/skill/Coding.java
+++ src/main/java/skill/Coding.java
@@ -11,8 +11,7 @@ import java.util.Arrays;
  */
 public class Coding {
 
-
-    /*
+    /**
     * 字符串切割
     * */
     @Test
@@ -23,5 +22,23 @@ public class Coding {
         System.out.println(Arrays.toString(split));
     }
 
+    /**
+     * 两数相除，向上取整
+    * */
+    @Test
+    public void test2() {
+        int a = 2;
+        int k = 2;
+        int res = (a + k - 1) / k;
+    }
 
+    /**
+    * 二分计算中点，使用位运算(运算符优先级)
+    * */
+    @Test
+    public void test3() {
+        int l = 0;
+        int r = 10;
+        int mid = l + ((r - l) >> 1);
+    }
 }
